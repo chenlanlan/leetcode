23dca5069b51ceccbdaa59c1749d63b21c198687
silan <silan0318@163.com>
2015-04-16 14:55:08 +0800

    best time to sell

---
+++ Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,18 @@
+#!/user/bin/python
+
+def maxProfit2(self, prices):
+    if len(prices) == 0:
+        return 0
+    min = prices[0]
+    profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] >= prices[i - 1]:
+            continue
+        profit += prices[i - 1] - min
+        min = prices[i]
+    profit += prices[len(prices) - 1] - min
+    return profit
+
+self = 0
+prices = [1, 2]
+print(maxProfit2(self, prices))
