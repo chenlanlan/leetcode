fcde0016d83e811c5d2fc133612143a263e1f4a7
silan <silan0318@163.com>
2015-05-20 18:26:57 +0800

    revision

--- Repeated DNA Sequences.py
+++ Repeated DNA Sequences.py
@@ -3,43 +3,21 @@
 import collections
 class Solution:
     # @param {string} s
-    # @return {string[]}
+    # @return {string[]}    
     def findRepeatedDnaSequences(self, s):
         n = len(s)
-        dict = {}
-        res = []
-        for i in range(n - 9):
-            if i < n - 10:
-                if s[i : i + 10] in dict and dict[s[i : i + 10]] == 1:
-                    res.append(s[i : i + 10])
-                    dict[s[i : i + 10]] += 1
-                elif s[i : i + 10] not in dict:
-                    dict[s[i : i + 10]] = 1
-            elif s[i: ] in dict and dict[s[i : i + 10]] == 1:
-                res.append(s[i : i + 10])
-                dict[s[i : i + 10]] += 1
-            else:
-                dict[s[i : i + 10]] = 1
-        return res
-    
-    def findRepeatedDnaSequences2(self, s):
-        n = len(s)
+        sequenceLength = 10
         count = collections.Counter()
         res = []
         for i in range(n - 9):
-            if i < n - 10:
-                if count[s[i : i + 10]] == 1:
-                    res.append(s[i : i + 10])
-                    count.update({s[i : i + 10]: 1})
-                elif count[s[i : i + 10]] == 0:
-                    count.update({s[i : i + 10]: 1})
-            elif count[s[i: ]] == 1: 
-                res.append(s[i: ])
-                count.update({s[i: ]: 1})
+            if count[s[i : i + sequenceLength]] == 1:
+                res.append(s[i : i + sequenceLength])
+                count.update({s[i : i + sequenceLength]: 1})
+            else:
+                count.update({s[i : i + sequenceLength]: 1})
         return res
 
 test = Solution()
 print(test.findRepeatedDnaSequences('AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT'))
-print(test.findRepeatedDnaSequences2('AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT'))
                 
                 
