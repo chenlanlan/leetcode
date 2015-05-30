7c3199d129936b0e6c8afba085ab6fc8e0f67cbb
silan <silan0318@163.com>
2015-05-30 10:14:55 +0800

    modified

--- Palindrome Partitioning.py
+++ Palindrome Partitioning.py
@@ -4,10 +4,7 @@ class Solution:
     # @param s, a string
     # @return a list of lists of string        
     def isPalindrome(self, s):
-        for i in range(len(s)):
-            if s[i] != s[len(s) - 1 - i]:
-                return False
-        return True
+        return s ==s[::-1]
 
     def dfs(self, s, res):
         if len(s) == 0:
