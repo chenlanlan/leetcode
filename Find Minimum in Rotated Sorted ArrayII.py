e8d0e0ebeca52f115e9258e5d622b191d1a7fae7
silan <silan0318@163.com>
2015-06-04 16:54:48 +0800

    modified

--- Find Minimum in Rotated Sorted ArrayII.py
+++ Find Minimum in Rotated Sorted ArrayII.py
@@ -22,8 +22,28 @@ class Solution:
             elif num[mid] > num[left]:
                 left = mid
             else:
-                right = mid 
+                right = mid
+        return num[left]
+    def findMin2(self, num):
+        left = 0
+        right = len(num) - 1
+        while left < right:
+            if num[left] < num[right]:
+                return num[left]
+            mid = (left + right) // 2
+            if num[mid] == num[left]:
+                if left + 1 == right:
+                    if num[left] >= num[right]:
+                        left = right
+                        break
+                else:
+                    left += 1
+            if num[left] < num[mid]:
+                left = mid
+            else:
+                right = mid
+            print(left, right)
         return num[left]
 
 x = Solution()
-print(x.findMin([1, 1]))
+print(x.findMin2([3, 3, 1]))
