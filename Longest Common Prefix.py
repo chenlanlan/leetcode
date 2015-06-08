a4269d11313a68be605c9fa459bc26250d8e08a7
silan <silan0318@163.com>
2015-06-08 22:13:20 +0800

    modified

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -8,9 +8,9 @@ class Solution:
             return ''
         for i in range (1, n):
             n1 = len(strs[0])
-            for j in range(0, n1):
+            for j in range(n1):
                 if len(strs[i]) < j + 1 or strs[i][j] != strs[0][j]:
-                    strs[0] = strs[0][0:j]
+                    strs[0] = strs[0][j]
                     break
         return strs[0]
 
--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -13,16 +13,12 @@ class Solution:
         node = root = ListNode(0)
         node1 = l1
         node2 = l2
-        while node1 and node2:
-            if node1.val < node2.val:
+        while node1 or node2:
+            if not node2 or node1 and node1.val < node2.val:
                 node.next = node1
                 node1 = node1.next
             else:
                 node.next = node2
                 node2 = node2.next
             node = node.next
-        if node1:
-            node.next = node1
-        else:
-            node.next = node2
         return root.next
