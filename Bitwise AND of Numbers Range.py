821f5c8837d6e1914c19f15beb10ec80c83da519
chenlanlan <silan0318@163.com>
2015-06-23 13:28:30 -0700

    modified

--- Bitwise AND of Numbers Range.py
+++ Bitwise AND of Numbers Range.py
@@ -5,23 +5,15 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def rangeBitwiseAnd(self, m, n):
-        if m == n:
-            return m
-        if m > n or n <= 0 or m <= 0:
-            return 0
-        mid = (m + n) // 2
-        return mid & self.rangeBitwiseAnd(m, mid - 1) & self.rangeBitwiseAnd(mid + 1, n)
-    def rangeBitwiseAnd2(self, m, n):
         if m == 0:
             return 0
-        move = 1
+        move = 0
         while m != n:
             m = m >> 1
             n = n >> 1
-            move = move << 1
-        return m * move
+            move += 1
+        return m << move
 
 x = Solution()
 print(x.rangeBitwiseAnd(5, 7))
-print(x.rangeBitwiseAnd2(2, 2147483647))
 
