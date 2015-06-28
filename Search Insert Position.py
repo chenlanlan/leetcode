96aeb3b5762ba2ade036264ed2eea255a9dada32
chenlanlan <silan0318@163.com>
2015-06-28 10:55:25 -0700

    new

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -5,16 +5,15 @@ class Solution:
     # @param {integer} target
     # @return {integer}
     def searchInsert(self, nums, target):
-        n = len(nums)
         left = 0
-        right = n - 1
+        right = len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
-            elif nums[mid] > target:
+            else:
                 right = mid - 1
         return left
 
