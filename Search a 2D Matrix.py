c580e1e70d133aff451ce27f33c780cdf49f6210
chenlanlan <silan0318@163.com>
2015-07-09 15:15:31 -0700

    modified

--- Search a 2D Matrix.py
+++ Search a 2D Matrix.py
@@ -7,39 +7,17 @@ class Solution:
     # @param {integer[][]} matrix
     # @param {integer} target
     # @return {boolean}
-    def binarySearch(self, A, left, right, key):
-        if A == []:
-            return False
-        while left < right:
-            mid = (left + right) // 2
-            if A[mid] == key:
-                return True
-            elif A[mid] < key:
-                left = mid + 1
-            elif A[mid] > key:
-                right = mid
-        return False
-    
     def searchMatrix(self, matrix, target):
-        n = len(matrix)
-        left = 0
-        right = n
-        rowIndex = -1
-        while left < right:
-            mid = (left + right) // 2
-            if matrix[mid][0] == target:
+        r = 0
+        c = len(matrix[0]) - 1
+        while r < len(matrix) and c >= 0:
+            if matrix[r][c] == target:
                 return True
-            elif left + 1 == right:
-                rowIndex = left
-                break
-            elif matrix[mid][0] < target:
-                left = mid
-            elif matrix[mid][0] > target:
-                right = mid
-        print (rowIndex)
-        if rowIndex != -1:
-            return self.binarySearch(matrix[rowIndex], 0, len(matrix[rowIndex]), target)
-        return False
+            if matrix[r][c] > target:
+                c -= 1
+            else:
+                r += 1
+    return False
 
 test = Solution()
 print(test.searchMatrix([
